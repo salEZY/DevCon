a973b33349fc074c454917bcb45c608060d39cfe
salEZY <bobicaleks@yahoo.com>
2019-08-21 16:29:32 +0200

    add routes for all profiles, by id and delete

--- server.js
+++ server.js
@@ -8,17 +8,14 @@ connectDB()
 // Middleware
 app.use(express.json({ extended: false }))
 
-app.get('/', (req, res) => {
-  res.send('API running')
-})
 // Import routes
 app.use('/api/users', require('./routes/api/users'))
 app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/profile', require('./routes/api/profile'))
 app.use('/api/posts', require('./routes/api/posts'))
 
+app.get('/', (req, res) => res.send('API running'))
+
 const port = process.env.port || 5000
 
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`)
-})
+app.listen(port, () => console.log(`Server started on port ${port}!`))
