9ba98d91b03c4987b28146239247270d69285f22
AleksBobic <bobicaleks@yahoo.com>
2019-08-25 20:32:41 +0200

    add login form

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,67 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
-  return <div>Login</div>
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  })
+
+  const { email, password } = formData
+
+  const onChange = e => setFormData({
+    ...formData, [e.target.name]: e.target.value
+  })
+
+  const onSubmit = async e => {
+    e.preventDefault()
+    
+      // const newUser = {
+      //   name, email, password
+      // }
+      // try {
+      //   const config = {
+      //     headers: {
+      //       'Content-Type': 'application/json'
+      //     }
+      //   }
+
+      //   const body = JSON.stringify(newUser)
+
+      //   const res = await axios.post('/api/users', body, config)
+      //   console.log(res.data);
+        
+      // } catch (error) {
+      //   console.error(error.response.data)
+      // }
+    console.log('Success!');
+  }
+
+  return <Fragment>
+    <section className="container">
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead"><i className="fas fa-user"></i> Sign Into Your Account</p>
+      <form className="form" onSubmit={e => onSubmit(e)}>
+        <div className="form-group">
+          <input type="email" placeholder="Email Address" name="email" value={email} onChange={e => onChange(e)} required/>
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            minLength="6" value={password} onChange={e => onChange(e)}
+            required
+          />
+        </div>
+        
+        <input type="submit" className="btn btn-primary" value="Sign in" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Create!</Link>
+      </p>
+    </section>
+  </Fragment>
 }
 
 export default Login
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -8,18 +9,35 @@ const Register = () => {
     password2: ''
   })
 
-  const { name, email, password, password2} = formData
+  const { name, email, password, password2 } = formData
 
   const onChange = e => setFormData({
     ...formData, [e.target.name]: e.target.value
   })
 
-  const onSubmit = e => {
+  const onSubmit = async e => {
     e.preventDefault()
     if (password !== password2) {
       console.log('Passwords do not match');
     }else{
-      console.log(formData);
+      // const newUser = {
+      //   name, email, password
+      // }
+      // try {
+      //   const config = {
+      //     headers: {
+      //       'Content-Type': 'application/json'
+      //     }
+      //   }
+
+      //   const body = JSON.stringify(newUser)
+
+      //   const res = await axios.post('/api/users', body, config)
+      //   console.log(res.data);
+        
+      // } catch (error) {
+      //   console.error(error.response.data)
+      // }
     }
   }
 
@@ -44,6 +62,7 @@ const Register = () => {
             placeholder="Password"
             name="password"
             minLength="6" value={password} onChange={e => onChange(e)}
+            required
           />
         </div>
         <div className="form-group">
@@ -52,12 +71,13 @@ const Register = () => {
             placeholder="Confirm Password"
             name="password2"
             minLength="6" value={password2} onChange={e => onChange(e)}
+            required
           />
         </div>
         <input type="submit" className="btn btn-primary" value="Register" />
       </form>
       <p className="my-1">
-        Already have an account? <a href="login.html">Sign In</a>
+        Already have an account? <Link to="/login">Sign In</Link>
       </p>
     </section>
   </Fragment>
