1b811258061fd80018b2ab488c373ffe81c0fa0b
salEZY <bobicaleks@yahoo.com>
2019-08-27 12:04:15 +0200

    alert component and action

--- client/src/App.js
+++ client/src/App.js
@@ -4,25 +4,27 @@ import Navbar from './components/layout/Navbar'
 import Landing from './components/layout/Landing'
 import Login from './components/auth/Login'
 import Register from './components/auth/Register'
+import Alert from './components/layout/Alert'
 import './App.css'
-// Redux 
+// Redux
 import { Provider } from 'react-redux'
 import store from './store'
 
 const App = () => (
   <Provider store={store}>
-  <Router>
-    <Fragment>
-      <Navbar />
-      <Route exact path="/" component={Landing} />
-      <section className="container">
-        <Switch>
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/login" component={Login} />
-        </Switch>
-      </section>
-    </Fragment>
-  </Router>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <section className="container">
+          <Alert />
+          <Switch>
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
   </Provider>
 )
 
