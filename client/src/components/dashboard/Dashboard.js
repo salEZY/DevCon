1154561e6320a06f1a3a4cf5c47c3e921a5cd08b
salEZY <bobicaleks@yahoo.com>
2019-09-03 15:34:26 +0200

    list education and experience

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -5,6 +5,8 @@ import { connect } from 'react-redux'
 import Spinner from '../layout/spinner'
 import { getCurrentProfile } from '../../actions/profile'
 import DashboardActions from './DashboardActions'
+import Experience from './Experience'
+import Education from './Education'
 
 const Dashboard = ({
   getCurrentProfile,
@@ -27,6 +29,8 @@ const Dashboard = ({
       {profile !== null ? (
         <Fragment>
           <DashboardActions />
+          <Experience experience={profile.experience} />
+          <Education education={profile.education} />
         </Fragment>
       ) : (
         <Fragment>
