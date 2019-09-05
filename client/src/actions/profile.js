98d8cc5c94c760839b2453e46cd624efba095bae
salEZY <bobicaleks@yahoo.com>
2019-09-05 12:00:43 +0200

    add display profiles

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -226,8 +226,7 @@ export const deleteEducation = id => async dispatch => {
 export const deleteAccount = () => async dispatch => {
   if (window.confirm('Are you sure? This can NOT be undone')) {
     try {
-      const res = await axios.delete(`/api/profile`)
-      console.log(res)
+      await axios.delete(`/api/profile`)
       dispatch({
         type: CLEAR_PROFILE
       })
