90c003be402dc76eaf58d84055b8dbf472808496
salEZY <bobicaleks@yahoo.com>
2019-09-09 12:20:41 +0200

    prepare for deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const connectDB = require('./config/db')
+const path = require('path')
 
 const app = express()
 // Connect MongoDB
@@ -14,8 +15,16 @@ app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/profile', require('./routes/api/profile'))
 app.use('/api/posts', require('./routes/api/posts'))
 
-app.get('/', (req, res) => res.send('API running'))
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'))
 
-const port = process.env.port || 5000
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
+const port = process.env.PORT || 5000
 
 app.listen(port, () => console.log(`Server started on port ${port}!`))
