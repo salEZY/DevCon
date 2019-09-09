639850c838a832516381a7e234005c51c51e4a67
salEZY <bobicaleks@yahoo.com>
2019-09-09 11:18:30 +0200

    add, delete comment

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -1,24 +1,33 @@
-import React, { Fragment, useEffect} from 'react'
+import React, { Fragment, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import Spinner from '../layout/spinner'
 import PostItem from '../posts/PostItem'
 import { getPost } from '../../actions/post'
 import { Link } from 'react-router-dom'
+import CommentForm from './CommentForm'
+import CommentItem from './CommentItem'
 
-const Post = ({
-  getPost,
-  post: { post, loading },
-  match
-}) => {
+const Post = ({ getPost, post: { post, loading }, match }) => {
   useEffect(() => {
     getPost(match.params.id)
-  }, [getPost]
+  }, [getPost, match.params.id])
+  return loading || post === null ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <Link to="/posts" className="btn">
+        Back to posts
+      </Link>
+      <PostItem post={post} showActions={false} />
+      <CommentForm postId={post._id} />
+      <div className="comments">
+        {post.comments.map(comment => (
+          <CommentItem key={comment._id} comment={comment} postId={post._id} />
+        ))}
+      </div>
+    </Fragment>
   )
-  return loading || post === null ? <Spinner /> : <Fragment>
-    <Link to="/posts" className="btn">Back to posts</Link>
-    <PostItem post={post} showActions={false} />
-  </Fragment>
 }
 
 Post.propTypes = {
@@ -30,4 +39,7 @@ const mapStateToProps = state => ({
   post: state.post
 })
 
-export default connect(mapStateToProps, { getPost })(Post)
+export default connect(
+  mapStateToProps,
+  { getPost }
+)(Post)
