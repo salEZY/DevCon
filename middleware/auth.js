8d391d84af271751b9fc977dfca6f82d5ded2d07
salEZY <bobicaleks@yahoo.com>
2019-09-12 15:50:30 +0200

    refactor based on env file

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,21 +1,21 @@
 const jwt = require('jsonwebtoken')
-const config = require('config')
+const { JWT } = require('../config/config')
 
 module.exports = (req, res, next) => {
   // get token from header
   const token = req.header('x-auth-token')
   // check if no token
-  if(!token) {
+  if (!token) {
     return res.status(401).json({
       msg: 'No token, denied!'
     })
   }
   // Verify token
   try {
-    const decoded = jwt.verify(token, config.get('jwtSecret'))
+    const decoded = jwt.verify(token, JWT)
     req.user = decoded.user
     next()
   } catch (error) {
-    res.status(401).json({ msg: 'Token is not valid!'})
+    res.status(401).json({ msg: 'Token is not valid!' })
   }
 }
